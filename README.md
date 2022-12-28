f04e0bc1461ad05024d7a92e60ce9b6189091083
wegfawefgawefg <668es218pur@gmail.com>
2022-12-28 16:04:44 +0900

    works sortof, rendering is bad

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # web-bruiser
+
 you will get bruises if you use this. not exactly code gold, but we aren't going deep here
+
+## feature
+
+- drop down with history search and auto complete on fuzzy string match in url bar?
+- forward and back on mouse press or keypress?
--- main.py
+++ main.py
@@ -2,9 +2,7 @@
 '''
 design docs:
 need a url bar, 
-need a back button,
-need a forward button,
-need a refresh button,
+need a refresh keypress,
 
 need a way to display the html
 need a way to display the images
@@ -14,12 +12,15 @@ clicking on a link should open the link in the same window
 
 only one tab is fine, no need for multiple tabs
 
-need a way to display the javascript
+need a way to display the javascript???
 '''
 
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QTextEdit
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit, QTextEdit, QMainWindow
 from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QVector2D
+import requests
+import re
 
 class WebBruiser(QWidget):
     def __init__(self):
@@ -27,29 +28,86 @@ class WebBruiser(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(0, 0, 600, 800)
+        # self.setGeometry(0, 0, 600, 800)
         self.setWindowTitle('Web Bruiser')
-        # add the url bar
-        self.urlBar = QLineEdit(self)
-        # add the back button
-        self.backButton = QPushButton('<', self)
-        # add the forward button
-        self.forwardButton = QPushButton('>', self)
-        # add a go button
-        self.goButton = QPushButton('Go', self)
 
-        # add the html display
-        self.htmlDisplay = QTextEdit(self)
+        self.bar_height = 30
+
+        self.url_bar = QLineEdit(self)
+        self.url_bar.setStyleSheet("background-color: #333333; color: #aaaaaa;")
+        self.url_bar.returnPressed.connect(self.go)
+
+        self.status_bar = QTextEdit(self)
+        self.status_bar.setReadOnly(True)
+
+        self.text = QTextEdit(self)
+        self.text.setReadOnly(True)
+        self.text.setText("home page")
+
+        self.resize_elements()
 
         self.show()
 
-    #quit on press q
+    def go(self):
+        # set status bar to "loading {url}"
+        self.status_bar.setText("loading " + self.url_bar.text())
+        url = self.url_bar.text()
+
+        try:
+            url = self.url_bar.text()
+            url = self.format_raw_url(url)
+            r = requests.get(url)
+            # if the url is invalid, set status bar to "invalid url"
+            if r.status_code != 200:
+                self.error_invalid_url()
+                return
+            elif r.status_code == 200:
+                self.status_bar.setText("loaded " + self.url_bar.text())
+                self.text.setText(r.text)
+        except Exception as e:
+            print(e)
+            self.error_invalid_url()
+        
+
+    def clear_url_bar(self):
+        is_default = self.url_bar.text().startswith("enter url")
+        print("is default: ", is_default)
+        if is_default:
+            self.url_bar.setText(self.url_bar.text()[len("enter url"):])
+
+    def error_invalid_url(self):
+        url = self.url_bar.text()
+        target_url = self.format_raw_url(url)
+        self.status_bar.setText(f"invalid url {target_url}")
+
+    def format_raw_url(self, url):
+        if not url.startswith("http://"):
+            url = "http://" + url
+        return url
+
+
     def keyPressEvent(self, e):
-        if e.key() == Qt.Key_Q:
+        if e.key() == Qt.Key_Q or e.key() == Qt.Key_Escape:
             self.close()
         # refresh on press r
         if e.key() == Qt.Key_R:
             self.refresh()
+
+    def resize_elements(self):
+        self.url_bar.resize(self.width(), self.bar_height)
+        self.url_bar.move(0, 0)
+        self.status_bar.resize(self.width(), self.bar_height)
+        self.status_bar.move(0, self.height() - self.bar_height)
+        self.text.resize(self.width(), self.height() - self.bar_height * 2)
+        self.text.move(0, self.bar_height)
+
+    def resizeEvent(self, event):
+        QMainWindow.resizeEvent(self, event)
+        self.resize_elements()
+
+
+
+
     
 if __name__ == '__main__':
     app = QApplication(sys.argv)
